7ca091d559a17ec778a0005f2dd8ca20adb92b32
Yolanda Fay <yolandafay@gmail.com>
2018-09-22 23:32:51 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,23 +5,6 @@ def key_for_min_value(name_hash)
   
   return nil if name_hash.empty? 
   
-  key_array = Array.new 
-  value_array = Array.new
-  i = 0 
   
-  name_hash.collect do |key, value|
-    key_array << key 
-    value_array << value 
-  end
-  
-  while i < value.length 
-    if value[i] < value[i+1]
-      smallest_value_index = i 
-    else
-      i += 1 
-    end
-  end
-  
-  return key_array[smallest_value_index]
     
 end
